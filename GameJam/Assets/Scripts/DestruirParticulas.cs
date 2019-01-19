bcc3c5202bf2111eba31286ae0d03152ed7a6585
Totopojr <46763040+Totopojr@users.noreply.github.com>
2019-01-19 12:52:42 -0600

    Test Tutorial y Personajes

---
+++ GameJam/Assets/Scripts/DestruirParticulas.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestruirParticulas : MonoBehaviour {
+
+    void Start()
+    {
+        //Destruir Particulas en el tiempo de vida maxima
+        Destroy(gameObject, GetComponent<ParticleSystem>().main.duration + GetComponent<ParticleSystem>().main.startLifetime.constantMax + GetComponent<ParticleSystem>().main.startDelay.constant);
+    }
+}
